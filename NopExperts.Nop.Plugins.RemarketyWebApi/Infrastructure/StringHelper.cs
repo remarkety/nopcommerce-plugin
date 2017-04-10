e4e2ba488c3739fdd847170659557458397990ff
Roman Shuvalov <romans@uteam.co.il>
2017-04-10 10:59:08 +0300

    UpdatedAt dateTime parsing fixed

--- NopExperts.Nop.Plugins.RemarketyWebApi/Infrastructure/StringHelper.cs
+++ NopExperts.Nop.Plugins.RemarketyWebApi/Infrastructure/StringHelper.cs
@@ -1,8 +1,29 @@
-﻿namespace NopExperts.Nop.Plugins.RemarketyWebApi.Infrastructure
+﻿using System;
+using System.Globalization;
+
+namespace NopExperts.Nop.Plugins.RemarketyWebApi.Infrastructure
 {
     public static class StringHelper
     {
         public static string RemarketyApiKey => "RemarketyApiKey";
         public static string RemarketyStoreIdSettingKey => "RemarketyWebApi.WebTracking.StoreId";
+
+        public static DateTime? ParseDateTime(string dateTimeString)
+        {
+            if (string.IsNullOrEmpty(dateTimeString))
+            {
+                return null;
+            }
+
+            DateTime result;
+
+            if (!DateTime.TryParseExact(dateTimeString, "yyyy-MM-ddTHH:mm:ss+0000", CultureInfo.InvariantCulture,
+                DateTimeStyles.AssumeLocal, out result))
+            {
+                return null;
+            }
+
+            return result;
+        }
     }
 }
