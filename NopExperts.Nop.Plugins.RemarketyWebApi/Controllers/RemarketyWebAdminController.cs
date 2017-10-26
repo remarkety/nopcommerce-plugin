da052ba8fceee01c8b5e86534473650cae46ea2c
Julia Trikoz <juliatrikoz7@gmail.com>
2017-10-26 15:11:14 +0300

    Add versions

--- NopExperts.Nop.Plugins.RemarketyWebApi/Controllers/RemarketyWebAdminController.cs
+++ NopExperts.Nop.Plugins.RemarketyWebApi/Controllers/RemarketyWebAdminController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using AutoMapper;
@@ -62,7 +63,9 @@ namespace NopExperts.Nop.Plugins.RemarketyWebApi.Controllers
                 {
                     DiscountNamePrefix = _remarketyDiscountsSettings.DiscountNamePrefix,
                     Enabled = _remarketyDiscountsSettings.Enabled
-                }
+                },
+
+                PluginVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString()
             };
 
             return View("~/Plugins/NopExperts.RemarketyWebApi/Views/RemarketyWebAdmin/Configure.cshtml", model);
